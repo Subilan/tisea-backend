2db1533474ad493beb7b9bbd390da5738a127e07
Subilan <suyuanliang1995@gmail.com>
2023-11-29 18:04:22 +0800

    middlewares

---
+++ middlewares/logger.go
@@ -0,0 +1,21 @@
+package middlewares
+
+import "github.com/gin-gonic/gin"
+import "fmt"
+import "time"
+
+func WithLogger() gin.HandlerFunc {
+	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.DateTime),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	})
+}
