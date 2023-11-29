d607e641177996d464b7df7287161aad776e76b8
Subilan <suyuanliang1995@gmail.com>
2023-11-29 22:37:50 +0800

    feat: request methods

---
+++ utils/request.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Respond(c *gin.Context, ok bool, finishMessage string, errorCause string, data interface{}) {
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"ok": ok,
+		"message": finishMessage,
+		"cause": errorCause,
+		"data": data,
+		"timestamp": time.Now().UnixMilli(),
+	})
+}
+
+func OK(c *gin.Context, finishMessage string, data interface{}) {
+	Respond(c, true, finishMessage, "", data)
+}
+
+func NG(c *gin.Context, errorCause string, data interface{}) {
+	Respond(c, false, "", errorCause, data)
+}
+
