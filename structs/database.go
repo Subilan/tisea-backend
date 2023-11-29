0b0406df44229b1c83c2cfcbf937614d6cda243e
Subilan <suyuanliang1995@gmail.com>
2023-11-29 22:40:21 +0800

    database (wip)

---
+++ structs/database.go
@@ -0,0 +1,5 @@
+package structs
+
+type DUser struct {
+	Username string `` 
+}
---
+++ utils/database.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func GetConnection() (*sql.DB, error) {
+	cfg := GetConfiguration()
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s:%d/%s", cfg.Database.Username, cfg.Database.Password, cfg.Database.Host, cfg.Database.Port, cfg.Database.Dbname))
+	if err != nil {
+		return nil, fmt.Errorf("Invalid connection arguments.")
+	}
+	connErr := db.Ping()
+	if connErr != nil {
+		return nil, fmt.Errorf("Cannot reach the database.")
+	}
+	db.SetConnMaxLifetime(3 * time.Minute)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	return db, nil
+}
+
+func Query(queryString string, values... interface{}) (sql.Result, error) {
+	db, err := GetConnection()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	stmt, stmtErr := db.Prepare(queryString)
+
+	if stmtErr != nil {
+		return nil, stmtErr
+	}
+
+	result, execErr := stmt.Exec(values...)
+	if execErr != nil {
+		return nil, execErr
+	}
+
+	return result, nil
+}
