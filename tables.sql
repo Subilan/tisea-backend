2c0827be1f8841bfd6a2b541cbf59a13c24e840c
Subilan <suyuanliang1995@gmail.com>
2023-12-01 22:53:23 +0800

    chore: sequence

--- tables.sql
+++ tables.sql
@@ -3,15 +3,15 @@ CREATE DATABASE tisea;
 USE tisea;
 
 CREATE TABLE `tisea_users` (
-	`id` INT unsigned NOT NULL AUTO_INCREMENT,
 	`username` VARCHAR(20) NOT NULL,
-	`password_hash` TEXT NOT NULL,
 	`email` VARCHAR(32) NOT NULL,
-	`level` INT unsigned NOT NULL DEFAULT 0,
-	`bio` VARCHAR(50) DEFAULT "",
+	`password_hash` TEXT NOT NULL,
 	`nickname` VARCHAR(20) DEFAULT "",
-	`group_id` INT unsigned NOT NULL DEFAULT 0,
+	`bio` VARCHAR(50) DEFAULT "",
+	`id` INT unsigned NOT NULL AUTO_INCREMENT,
 	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+	`group_id` INT unsigned NOT NULL DEFAULT 0,
+	`level` INT unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`)
 );
