921e5c469e5ec8d32296ddc8d3df04298ebea561
Subilan <suyuanliang1995@gmail.com>
2023-12-02 15:29:06 +0800

    feat: login

--- routes/auth/login.go
+++ routes/auth/login.go
@@ -1,2 +1,47 @@
 package auth
 
+import (
+	"fmt"
+	"tisea-backend/structs"
+	"tisea-backend/utils/database"
+	"tisea-backend/utils/response"
+	"tisea-backend/utils/security"
+
+	"github.com/gin-gonic/gin"
+)
+
+func login(ctx *gin.Context) {
+	var request structs.LoggingInUser
+
+	if err := ctx.BindJSON(&request); err != nil {
+		response.NG(ctx, err, nil)
+		return
+	}
+
+	if len(request.Username) == 0 || len(request.Password) == 0 {
+		response.NG(ctx, fmt.Errorf("NOT_ENOUGH_ARGUMENT"), nil)
+		return
+	}
+
+	user, userErr := database.GetUserByUsername(request.Username)
+
+	if user == nil {
+		if userErr == nil {
+			response.NG(ctx, fmt.Errorf("NOT_FOUND"), nil)
+		} else {
+			response.NG(ctx, userErr, nil)
+		}
+		return
+	}
+
+	if security.CompareHash(request.Password, user.Hash) {
+		token, tokenErr := security.GenerateTokenForUser(request.Username, request.Remembered)
+		if tokenErr != nil {
+			response.NG(ctx, tokenErr, nil)
+		} else {
+			response.OK(ctx, "User successfully logged in.", token)
+		}
+	} else {
+		response.NG(ctx, fmt.Errorf("INVALID_CREDENTIALS"), nil)
+	}
+}
