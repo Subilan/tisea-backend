d62271eb97370f5aef56823cdbef6e3975950598
Subilan <suyuanliang1995@gmail.com>
2023-12-02 15:29:16 +0800

    feat: register

--- routes/auth/register.go
+++ routes/auth/register.go
@@ -1,21 +1,49 @@
 package auth
 
 import (
+	"fmt"
 	"tisea-backend/structs"
-	"tisea-backend/utils"
+	"tisea-backend/utils/database"
+	"tisea-backend/utils/response"
 
 	"github.com/gin-gonic/gin"
 )
 
-func handle(ctx *gin.Context) {
-	var request structs.RUserRegister
+func register(ctx *gin.Context) {
+	var request structs.RegisteringUser
 
 	if err := ctx.BindJSON(&request); err != nil {
-		utils.NG(ctx, err.Error(), nil)
+		response.NG(ctx, err, nil)
 		return
 	}
 
 	if len(request.Username) == 0 || len(request.Email) == 0 || len(request.Password) == 0 {
-		utils.NG(ctx, "Not enough argument.", nil)
+		response.NG(ctx, fmt.Errorf("NOT_ENOUGH_ARGUMENT"), nil)
+		return
 	}
+
+	if user, userErr := database.GetUserByUsername(request.Username); user != nil {
+		if userErr == nil {
+			response.NG(ctx, fmt.Errorf("DUPLICATE"), nil)
+		} else {
+			response.NG(ctx, userErr, nil)
+		}
+		return
+	}
+
+	registering, registeringErr := database.MakeRegisteringUser(request.Username, request.Email, request.Password)
+
+	if registeringErr != nil {
+		response.NG(ctx, registeringErr, nil)
+		return
+	}
+
+	insertErr := database.InsertRegisteringUser(*registering)
+
+	if insertErr != nil {
+		response.NG(ctx, insertErr, nil)
+		return
+	}
+
+	response.OK(ctx, "Registration finished successfully.", nil)
 }
