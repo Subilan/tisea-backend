1f404aa976b0b24ba323921e0ea760cbd90af22b
Subilan <suyuanliang1995@gmail.com>
2023-12-03 11:23:36 +0800

    chore: redundant

--- cli/cli.go
+++ cli/cli.go
@@ -1,16 +1,22 @@
 package cli
 
-import cliv2 "github.com/urfave/cli/v2"
+import (
+	// "os"
+	// "os/signal"
+	// "syscall"
+
+	cliv2 "github.com/urfave/cli/v2"
+)
 
 var Commands = []*cliv2.Command{
-	&cliv2.Command{
+	{
 		Name: "help",
 		Usage: "Get help messages",
 		Action: func(ctx *cliv2.Context) error {
 			return cliv2.ShowAppHelp(ctx)
 		},
 	},
-	&cliv2.Command{
+	{
 		Name: "run",
 		Usage: "Run the backend",
 		Action: func(ctx *cliv2.Context) error {
