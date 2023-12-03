dd6070a520221081489ef75fa342040f5db1f6b0
Subilan <suyuanliang1995@gmail.com>
2023-12-03 11:23:47 +0800

    chore: add calling

--- main.go
+++ main.go
@@ -3,19 +3,22 @@ package main
 import (
 	"log"
 	"os"
-	"github.com/urfave/cli/v2"
+	"tisea-backend/cli"
+
+	cliv2 "github.com/urfave/cli/v2"
 )
 
 func main() {
-	app := &cli.App{
+	app := &cliv2.App{
 		Name: "tisea",
 		Usage: "Central management and entrance of the backend part of Tisea.",
-		Action: func(ctx *cli.Context) error {
+		Action: func(ctx *cliv2.Context) error {
 			if ctx.NArg() == 0 {
-				return cli.ShowAppHelp(ctx)
+				return cliv2.ShowAppHelp(ctx)
 			}
 			return nil
 		},
+		Commands: cli.Commands,
 	}
 
 	if err := app.Run(os.Args); err != nil {
