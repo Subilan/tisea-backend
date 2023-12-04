2de3ad20a74bb7f467d457222a181288c8834cc5
Subilan <suyuanliang1995@gmail.com>
2023-12-04 19:58:17 +0800

    chore: scanning types

--- structs/user.go
+++ structs/user.go
@@ -1,17 +1,19 @@
 package structs
 
+import "time"
+
 // 存储在数据库中的完整 User 对象
 type DatabaseUser struct {
-	Username  string `json:"username"`
-	Email     string `json:"email"`
-	Hash      string `json:"hash"`
-	Nickname  string `json:"nickname"`
-	Bio       string `json:"bio"`
-	ID        uint32 `json:"id"`
-	CreatedAt int64  `json:"created_at"`
-	UpdatedAt int64  `json:"updated_at"`
-	GroupId   uint8  `json:"group_id"`
-	Level     uint8  `json:"level"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Hash      string    `json:"hash"`
+	Nickname  string    `json:"nickname"`
+	Bio       string    `json:"bio"`
+	ID        uint32    `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	GroupId   uint8     `json:"group_id"`
+	Level     uint8     `json:"level"`
 }
 
 // 包含基本信息的 User 对象
