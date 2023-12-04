7d3d20d213f8fa4ae846fe614180a4fce370f89b
Subilan <suyuanliang1995@gmail.com>
2023-12-04 22:05:36 +0800

    fix: using more appropriate data types

--- structs/dynamic.go
+++ structs/dynamic.go
@@ -1,21 +1,23 @@
 package structs
 
+import "time"
+
 type DatabaseDynamic struct {
-	ID         uint64   `json:"id"`
-	Title      string   `json:"title"`
-	Content    string   `json:"content"`
-	Author     string   `json:"author"`
-	Hidden     bool     `json:"hidden"`
-	Categories []string `json:"categories"`
-	Tags       []string `json:"tags"`
-	CreatedAt  int64    `json:"created_at"`
-	UpdatedAt  int64    `json:"updated_at"`
+	ID         uint64    `json:"id"`
+	Title      string    `json:"title"`
+	Content    string    `json:"content"`
+	Author     string    `json:"author"`
+	Hidden     bool      `json:"hidden"`
+	Categories string  `json:"categories"`
+	Tags       string  `json:"tags"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
 
 type PostingDynamic struct {
 	Title      string   `json:"title"`
 	Content    string   `json:"content"`
 	Author     string   `json:"author"`
-	Categories []string `json:"categories"`
-	Tags       []string `json:"tags"`
+	Categories string `json:"categories"`
+	Tags       string `json:"tags"`
 }
