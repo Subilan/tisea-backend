a4e89c296e5cfe44fb685fdb036514ee39583e4c
Subilan <suyuanliang1995@gmail.com>
2023-12-05 20:07:14 +0800

    chore

--- structs/dynamic.go
+++ structs/dynamic.go
@@ -8,16 +8,16 @@ type DatabaseDynamic struct {
 	Content    string    `json:"content"`
 	Author     string    `json:"author"`
 	Hidden     bool      `json:"hidden"`
-	Categories string  `json:"categories"`
-	Tags       string  `json:"tags"`
+	Categories string    `json:"categories"`
+	Tags       string    `json:"tags"`
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
 }
 
 type PostingDynamic struct {
-	Title      string   `json:"title"`
-	Content    string   `json:"content"`
-	Author     string   `json:"author"`
+	Title      string `json:"title"`
+	Content    string `json:"content"`
+	Author     string `json:"author"`
 	Categories string `json:"categories"`
 	Tags       string `json:"tags"`
 }
