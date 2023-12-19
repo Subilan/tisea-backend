a9b10504ee907f94903b5c343c6b9e3fb30500eb
Subilan <suyuanliang1995@gmail.com>
2023-12-19 21:13:05 +0800

    chore: more RESTful

--- handlers/dynamic/dynamic.go
+++ handlers/dynamic/dynamic.go
@@ -5,8 +5,8 @@ import "github.com/gin-gonic/gin"
 func Bind(gin *gin.Engine) {
 	dynamics := gin.Group("/dynamics")
 
-	dynamics.POST("/create")
-	dynamics.POST("/delete")
+	dynamics.POST("/", create)
+	dynamics.DELETE("/", delete)
 	dynamics.POST("/hide")
 	dynamics.GET("/")
 }
